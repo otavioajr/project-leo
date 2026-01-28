b855e12639c5deff1b6714eb00dc84d97660e32d
Otavio Junior <otavioajr@gmail.com>
2026-01-28 14:52:22 +0000

    Try fixing this error: `Runtime ReferenceError: Link is not defined. Err

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useAuth } from '@/firebase';
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
