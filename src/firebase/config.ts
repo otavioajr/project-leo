c19ae10067b185b113b91d5a916f634eb3f22b2b
Otavio Junior <otavioajr@gmail.com>
2026-01-28 14:34:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5478905902-1fee1",
+  "appId": "1:261526029736:web:6ed8fdc67c0934aeb0099d",
+  "apiKey": "AIzaSyDT7wiGMLbGCGUQV5gBAEtPUkTVJPMlJPM",
+  "authDomain": "studio-5478905902-1fee1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "261526029736"
+};
